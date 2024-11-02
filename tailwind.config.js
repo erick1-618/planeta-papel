0d750a94e2f1488a54be84b39c98725e46b6f51c
Gustavo  Barros <gustavoandrade0125@gmail.com>
2024-11-02 17:19:49 -0300

    model: add basic template for pages

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      width: {
+        'desktop': '40%',      // Largura padrão para telas maiores
+        'mobile': '100%',       // Largura que ocupa a tela inteira em dispositivos móveis
+      },
+
+      colors: {
+        primaryBlue: '#3B87FA',      // Azul primário
+        secondaryBlue: '#2A5EAD',    // Azul secundário
+        accentBlue: '#1D437A',       // Azul de destaque
+        gray: '#E6E6E7',             // Cinza
+        dark: '#14141C',             // Cinza claro
+      },
+    },
+  },
+  plugins: [],
+}
